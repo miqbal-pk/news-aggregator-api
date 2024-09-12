3a19d09a83db2a832f04a27724807d7b290fc0b3
iqbalaabi <iqbalaabi01@gmail.com>
2024-09-12 23:05:26 +0500

    Made Articles seeder to generate 10 random articles

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\Article;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    protected $model = Article::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+
+            'source_id' =>fake()->words(3, true),
+            'source_name' => fake()->words(3, true), 
+            'author' => fake()->name(),
+            'category'=> fake()->word(),
+            'title' => fake()->sentence(),
+            'description' =>fake()->paragraphs(2, true),
+            'url'=> fake()->url(),
+            'url_to_image' => fake()->imageUrl(), 
+            'content' =>fake()->realText(700),
+            'published_at' => now(),
+        ];
+    }
+}
