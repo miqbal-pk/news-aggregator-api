7ff49b9cc90c93ab10ef2774157eeea8e7982bcf
iqbalaabi <iqbalaabi01@gmail.com>
2024-09-20 12:05:59 +0500

    scheduled the command

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
+use App\Console\Commands\FetchArticles;
+use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('app:fetch-articles')->daily();
